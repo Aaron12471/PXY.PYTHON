a926f71dae575e92cbe2b7ea3c33466ff9a3f497
Isaac_#8917 <103591767+Isaac-8917@users.noreply.github.com>
2023-03-21 13:47:39 -0500

    Code that u wanted me to put but it also wont work
    bruh

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+def proxy(path):
+    url = request.args.get('url')
+    if not url:
+        return 'Error: No URL specified.'
+
+    if request.method == 'GET':
+        response = requests.get(url)
+    elif request.method == 'POST':
+        response = requests.post(url, data=request.form)
+    elif request.method == 'PUT':
+        response = requests.put(url, data=request.form)
+    elif request.method == 'DELETE':
+        response = requests.delete(url)
+    elif request.method == 'PATCH':
+        response = requests.patch(url, data=request.form)
+
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in response.raw.headers.items()
+               if name.lower() not in excluded_headers]
+    return response.content, response.status_code, headers
